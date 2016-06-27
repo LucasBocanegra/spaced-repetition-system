b312a61e31140171584b40b407e2be12958be9c6
LucasBocanegra <lucasfernandobocanegra@gmail.com>
2016-06-27 09:57:57 -0300

    create view init_review, update template base, and update model Cards

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import  include,url
 from . import  views
 
 urlpatterns = [
-    url(r'^$', views.deck_list),
+    url(r'^$', views.my_decks),
     url(r'^mydecks/', views.my_decks, name='my_decks'),
     url(r'^deck/new/$', views.deck_new, name='deck_new'),
     url(r'^deck/(?P<pk>[0-9]+)/edit/$', views.deck_edit, name='deck_edit'),
     url(r'^deck/(?P<pk>[0-9]+)/detail/$', views.deck_detail, name='deck_detail'),
     url(r'^deck/(?P<pk>[0-9]+)/delete/$', views.deck_delete, name='deck_delete'),
+    url(r'^cards/init/(?P<pk>[0-9]+)$', views.init_review, name='init_review'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Deck
+from .models import Deck, Card
 from .forms import DeckForm
 from django.shortcuts import get_object_or_404
-
-
-def deck_list(request):
-    return render(request, 'app/deck_list.html', {})
 
 
 def my_decks(request):
@@ -47,6 +43,7 @@ def deck_detail(request, pk):
     deck = get_object_or_404(Deck, pk=pk)
     return render(request, 'app/deck_detail.html', {'deck': deck})
 
+
 def deck_delete(request, pk):
     deck = get_object_or_404(Deck, pk=pk)
     if request.method == "POST":
@@ -61,3 +58,9 @@ def deck_delete(request, pk):
         deck.delete()
         decks = Deck.objects.all()
         return render(request, 'app/my_decks.html', {'decks': decks})
+
+
+def init_review(request, pk):
+    cards = Card.objects.filter(deck=pk)
+
+    return render(request, 'app/init_review.html', {'cards': cards})
